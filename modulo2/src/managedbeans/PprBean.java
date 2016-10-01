0cbf2e9c33fe36ab3244b3201a6a03eaacf86b72
Aluno Usuario <aluno@softville.org>
2016-10-01 11:58:57 -0300

    Primefaces

---
+++ modulo2/src/managedbeans/PprBean.java
@@ -0,0 +1,10 @@
+package managedbeans;
+
+public class PprBean {
+	private String firstname;   
+	
+	public String getFirstname() {   return firstname;  } 
+	 
+	public void setFirstname(String firstname) {   this.firstname = firstname;  } 
+	 
+}
